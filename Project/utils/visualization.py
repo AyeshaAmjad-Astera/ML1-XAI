6687bd45ca77f399b6b30a2f96e9df19209b768d
10284324+AzeemQidwai@users.noreply.github.com <10284324+AzeemQidwai@users.noreply.github.com>
2023-05-28 19:28:48 +0500

    visualization updated

--- Project/utils/visualization.py
+++ Project/utils/visualization.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import missingno as msno
+from sklearn.metrics import roc_curve, auc, classification_report, confusion_matrix
 
 sns.set_theme()
 
@@ -33,3 +34,21 @@ def dist_plot(df: pd.DataFrame, x: str, y: str):
 
     sns.histplot(df, x = x, kde = True, hue = y, ax = ax1)
     sns.boxplot(df, y = x, x = y, orient='v', ax = ax2)
+
+def plot_roc(model, X_test, y_test, model_name):
+    y_pred = model.predict(X_test)
+    y_pred_proba = model.predict_proba(X_test)[:, 1]
+    print(classification_report(y_test, y_pred))
+    cm = confusion_matrix(y_test, y_pred)
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+    roc_auc = auc(fpr, tpr)
+    fig = plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate', fontsize = 14)
+    plt.ylabel('True Positive Rate', fontsize = 14)
+    plt.title(f'Receiver Operating Characteristic {model_name}', fontsize = 14)
+    plt.legend(loc="lower right")
